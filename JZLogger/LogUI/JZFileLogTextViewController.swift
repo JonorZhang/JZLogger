57527ec601742a9adbf042239fb5cddea74e4068
JonorZhang <824538183@qq.com>
2019-08-23 00:13:16 +0800

    优化logtextVc

--- JZLogger/LogUI/JZFileLogTextViewController.swift
+++ JZLogger/LogUI/JZFileLogTextViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 @objc public class JZFileLogTextViewController: UIViewController {
     
+    private let kFontSizeKey = "key.FontSize.JZFileLogTextViewController"
+    
     var logFileURL: URL!
     
     @IBOutlet weak var regExpBtn: UIButton!
@@ -30,48 +32,73 @@ import UIKit
     
     @IBOutlet weak var toolbarBottomConstraint: NSLayoutConstraint!
     
+    @IBOutlet weak var moreSettingPanel: UIView!
+    
+    @IBOutlet weak var fontSizeSlider: UISlider!
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
-        let data = JZFileLogger.shared.readLogFile(logFileURL) ?? Data()
-        textView?.text = String(data: data, encoding: .utf8)
-        textView?.isEditable = false
-        textView?.textColor = .black
         
+        let data = JZFileLogger.shared.readLogFile(logFileURL) ?? Data()
+        textView.text = String(data: data, encoding: .utf8)
+        textView.isEditable = false
+        textView.textColor = .black
+        if let fontSize = UserDefaults.standard.value(forKey: kFontSizeKey) as? CGFloat {
+            textView.font = .systemFont(ofSize: fontSize)
+            fontSizeSlider.value = Float(fontSize)
+        }
+
         observeKeyboard()
     }
     
     deinit {
         NotificationCenter.default.removeObserver(self)
-        print(#function)
+        print(#function, self.classForCoder)
     }
     
     func observeKeyboard() {
-        NotificationCenter.default.addObserver(forName: UIApplication.keyboardWillShowNotification, object: nil, queue: nil) { (note) in
-            print(#function, note.userInfo)
+        NotificationCenter.default.addObserver(forName: UIApplication.keyboardWillShowNotification, object: nil, queue: nil) { [unowned self](note) in
             if let endFrame = note.userInfo?["UIKeyboardFrameEndUserInfoKey"] as? CGRect {
                 let dy = endFrame.origin.y - self.toolbarView.frame.origin.y - self.toolbarView.frame.size.height
                 self.toolbarBottomConstraint.constant = dy
+                self.view.layoutIfNeeded()
             }
         }
-        NotificationCenter.default.addObserver(forName: UIApplication.keyboardWillHideNotification, object: nil, queue: nil) { (note) in
-            print(#function, note.userInfo)
+        NotificationCenter.default.addObserver(forName: UIApplication.keyboardWillHideNotification, object: nil, queue: nil) { [unowned self](note) in
             self.toolbarBottomConstraint.constant = 0
+            self.view.layoutIfNeeded()
         }
     }
     
     @IBAction func regExpClicked(_ sender: Any) {
         print(#function)
     }
+    
     @IBAction func caseSensitiveClicked(_ sender: Any) {
         print(#function)
     }
+    
     @IBAction func wholeWordClicked(_ sender: Any) {
         print(#function)
     }
+    
     @IBAction func filterClicked(_ sender: Any) {
         print(#function)
     }
+    
     @IBAction func moreSettingClicked(_ sender: Any) {
-        print(#function)
+        let sx: CGFloat = 0.9, sy: CGFloat = 0.3
+        let tx = moreSettingPanel.frame.size.width * (1.0 - sx) / 2.0
+        let ty = moreSettingPanel.frame.size.height * (1.0 - sy) / 2.0
+        moreSettingPanel.transform = CGAffineTransform(a: sx, b: 0, c: 0, d: sy, tx: tx, ty: ty)
+        UIView.animate(withDuration: 0.3) {
+            self.moreSettingPanel.isHidden = !self.moreSettingPanel.isHidden
+            self.moreSettingPanel.transform = .identity
+        }
+    }
+    
+    @IBAction func fontSizeValueChanged(_ sender: UISlider) {
+        UserDefaults.standard.set(sender.value, forKey: kFontSizeKey)
+        textView.font = .systemFont(ofSize: CGFloat(sender.value))
     }
 }
