889ced9b30b3d4f498cb4f2e7d5a7cda0f35bd8c
JonorZhang <824538183@qq.com>
2019-08-26 08:02:01 +0800

    优化日志显示

--- JZLogger/LogUI/JZFileLogTextViewController.swift
+++ JZLogger/LogUI/JZFileLogTextViewController.swift
@@ -42,12 +42,18 @@ import UIKit
     
     private var matcheResults: (curIdx: Int, all: [NSTextCheckingResult]?) = (0, nil) {
         didSet {
-            guard let resultAll = matcheResults.all else { return }
-            textView.textStorage.setAttributes([.backgroundColor : UIColor.yellow], range: resultAll[oldValue.curIdx].range)
+            guard let resultAll = matcheResults.all, resultAll.count > 0 else { return }
+            
+            // 1. 如果是prev/next进来需要还原当前文本为未选中颜色
+            if let oldResultAll = oldValue.all, oldResultAll == resultAll {
+                textView.textStorage.addAttributes([.backgroundColor : UIColor.yellow], range: oldResultAll[oldValue.curIdx].range)
+            }
+            // 2. 更新prev/next文本为选中颜色
             let range = resultAll[matcheResults.curIdx].range
             resultCountLabel.text = "\(matcheResults.curIdx + 1)/\(resultAll.count)"
             textView.scrollRangeToVisible(range)
-            textView.textStorage.setAttributes([.backgroundColor : UIColor.red], range: range)
+            textView.textStorage.addAttributes([.backgroundColor : UIColor.red], range: range)
+            if matcheResults.curIdx == 0 { makeToast("第一个") }
         }
     }
     
@@ -78,7 +84,7 @@ import UIKit
             guard let `self` = self else { return }
             if let endFrame = note.userInfo?["UIKeyboardFrameEndUserInfoKey"] as? CGRect {
                 let dy = endFrame.origin.y - self.toolbarView.frame.origin.y - self.toolbarView.frame.size.height
-                self.toolbarBottomConstraint.constant = dy
+                self.toolbarBottomConstraint.constant += dy
                 self.view.layoutIfNeeded()
             }
         }
@@ -86,6 +92,31 @@ import UIKit
             guard let `self` = self else { return }
             self.toolbarBottomConstraint.constant = 0
             self.view.layoutIfNeeded()
+        }
+    }
+    
+    func makeToast(_ msg: String) {
+        let alert = UIAlertController(title: nil, message: "无搜索结果", preferredStyle: .alert)
+        present(alert, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: .now()+1, execute: {
+            alert.dismiss(animated: true)
+        })
+    }
+    
+    func search(in string: String, pattern: String, regExp: NSRegularExpression) {
+        let strRange = NSRange(location: 0, length: string.count)
+        let results = regExp.matches(in: string, options: .reportCompletion, range: strRange)
+        print(pattern, strRange, results.count, results)
+        textView.textStorage.removeAttribute(.backgroundColor, range: strRange)
+        results.forEach({ (res) in
+            textView.textStorage.addAttributes([.backgroundColor : UIColor.yellow], range: res.range)
+        })
+        searchResultView.isHidden = !(results.count > 0)
+
+        if results.count > 0 {
+            matcheResults = (0, results)
+        } else {
+            makeToast("无搜索结果")
         }
     }
     
@@ -114,6 +145,7 @@ import UIKit
         let tx = moreSettingPanel.frame.size.width * (1.0 - sx) / 2.0
         let ty = moreSettingPanel.frame.size.height * (1.0 - sy) / 2.0
         moreSettingPanel.transform = CGAffineTransform(a: sx, b: 0, c: 0, d: sy, tx: tx, ty: ty)
+        view.bringSubviewToFront(moreSettingPanel)
         UIView.animate(withDuration: 0.3) {
             self.moreSettingPanel.isHidden = !self.moreSettingPanel.isHidden
             self.moreSettingPanel.transform = .identity
@@ -126,17 +158,26 @@ import UIKit
     }
     
     @IBAction func prevSearchResultClicked(_ sender: Any) {
-        guard matcheResults.curIdx > 0, let _ = matcheResults.all else { return }
-        matcheResults.curIdx -= 1
+        guard let resultAll = matcheResults.all else { return }
+        if matcheResults.curIdx > 0 {
+            matcheResults.curIdx -= 1
+        } else {
+            matcheResults.curIdx = resultAll.count - 1
+        }
     }
     
     @IBAction func nextSearchResultClicked(_ sender: Any) {
-        guard let resultAll = matcheResults.all, matcheResults.curIdx < resultAll.count - 1 else { return }
-        matcheResults.curIdx += 1
+        guard let resultAll = matcheResults.all else { return }
+        if matcheResults.curIdx < resultAll.count - 1 {
+            matcheResults.curIdx += 1
+        } else {
+            matcheResults.curIdx = 0
+        }
     }
     
     @IBAction func toggleSearchResultViewClicked() {
         searchResultView.isHidden = !searchResultView.isHidden
+        view.bringSubviewToFront(searchResultView)
         if let string = self.textView.text, searchResultView.isHidden {
             let strRange = NSRange(location: 0, length: string.count)
             self.textView.textStorage.removeAttribute(.backgroundColor, range: strRange)
@@ -148,28 +189,12 @@ extension JZFileLogTextViewController: UISearchBarDelegate {
     
     public func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
-        
-        if let string = self.textView.text, let pattern = self.searchBar.text {
-            var options: NSRegularExpression.Options = []
-            if !self.caseSensitiveBtn.isSelected { options.insert(.caseInsensitive) }
-            if self.wholeWordBtn.isSelected { options.insert(.useUnicodeWordBoundaries) }
-            
+        if let string = textView.text, var pattern = searchBar.text {
+            var options: NSRegularExpression.Options = [.anchorsMatchLines]
+            if !caseSensitiveBtn.isSelected { options.insert(.caseInsensitive) }
+            if wholeWordBtn.isSelected { pattern = #"\b\#(pattern)\b"# }
             if let regExp = try? NSRegularExpression(pattern: pattern, options: options) {
-                DispatchQueue.global().async {
-                    let strRange = NSRange(location: 0, length: string.count)
-                    let results = regExp.matches(in: string, options: .reportCompletion, range: strRange)
-                    print(pattern, strRange, results.count, results)
-                    DispatchQueue.main.async {
-                        self.textView.textStorage.removeAttribute(.backgroundColor, range: strRange)
-                        results.forEach({ (res) in
-                            self.textView.textStorage.addAttribute(.backgroundColor, value: UIColor.yellow, range: res.range)
-                        })
-                        if results.count > 0 {
-                            self.toggleSearchResultViewClicked()
-                        }
-                        self.matcheResults = (0, results)
-                    }
-                }
+                search(in: string, pattern: pattern, regExp: regExp)
             }
         }
     }
