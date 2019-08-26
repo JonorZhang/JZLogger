ed5b4bc5fec05da744a3c4f8f499521c2530b1c4
JonorZhang <824538183@qq.com>
2019-08-27 00:24:46 +0800

    删除注释

--- JZLogger/LogUI/JZFileLogListTableViewController.swift
+++ JZLogger/LogUI/JZFileLogListTableViewController.swift
@@ -16,22 +16,15 @@ class JZFileLogListTableViewController: UITableViewController {
         super.viewDidLoad()
         
         title = "日志列表"
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     // MARK: - Table view data source
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         print("numberOfRowsInSection:\(section)", dataSource.count)
         return dataSource.count
     }
@@ -43,13 +36,11 @@ class JZFileLogListTableViewController: UITableViewController {
         return cell
     }
     
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
     
     
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             JZFileLogger.shared.deleteLogFile(dataSource[indexPath.row])
